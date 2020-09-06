4a0bbf9ffe8461803f27564484da78d0480eebee
GluhovDmitry <dominges100@yandex.ru>
2020-09-06 11:34:51 +0000

    addded wsgi

---
+++ hello.py
@@ -0,0 +1,6 @@
+def wsgi_application(environ, start_response):
+	status = '200 OK'
+	headers = [('Content-Type','text/plain')]
+	body = 'Hello World!'
+	start_response(status, headers)
+	return [body]
