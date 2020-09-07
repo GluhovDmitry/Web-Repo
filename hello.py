0a272dda3cfe386e916f38151c6d870ef64997ef
GluhovDmitry <dominges100@yandex.ru>
2020-09-07 17:42:33 +0500

    omg

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
-def wsgi_application(environ, start_response):
-	status = '200 OK'
-	headers = [('Content-Type','text/plain')]
-	return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding = "utf-8")]
-	
+
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
